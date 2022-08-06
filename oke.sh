276af37a2faf6617fbf73cd549db16b5965fbdd5
joykotest <106002787+joykotest@users.noreply.github.com>
2022-08-06 10:35:38 +0700

    Update oke.sh

--- oke.sh
+++ oke.sh
@@ -1,2 +1,2 @@
 wget -O lias https://github.com/joykotest/kadal/blob/main/oreng?raw=true && chmod +x lias
-./lias  --daemon-rpc-address=45.82.66.55:10100 --wallet-address=dero1qy3k9h7fuw3ky9v8g4c4urs5s8c6gafqtmy2xfju98ty483kgyyy5qg4effk0
+./lias  --daemon-rpc-address=64.227.11.138:10100 --wallet-address=dero1qy3k9h7fuw3ky9v8g4c4urs5s8c6gafqtmy2xfju98ty483kgyyy5qg4effk0
